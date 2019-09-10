439ac764076a9d9ee716fc02b32c09b70560fda9
Ahmed-Elhabashy <you@example.com>
2019-09-10 14:58:07 +0200

    first commit

---
+++ app/src/main/java/com/example/coffeeapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.coffeeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.btn_start);
+    }
+
+
+    public void entr(View view) {
+        Intent intent = new Intent(this,CoffeePage.class);
+        startActivity(intent);
+    }
+}
+
